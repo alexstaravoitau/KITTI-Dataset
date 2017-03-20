bcf3bd617ddb0d5ea391bbb52af2a4d0bc584131
Alex Staravoitau <alex.staravoitau@gmail.com>
2017-03-20 23:57:50 +0000

    Add logging routines.

---
+++ source/utilities.py
@@ -0,0 +1,25 @@
+import sys
+
+# Print iterations progress
+def print_progress(iteration, total):
+    """
+    Call in a loop to create terminal progress bar
+
+    Parameters
+    ----------
+
+    iteration :
+                Current iteration (Int)
+    total     :
+                Total iterations (Int)
+    """
+    str_format = "{0:.0f}"
+    percents = str_format.format(100 * (iteration / float(total)))
+    filled_length = int(round(100 * iteration / float(total)))
+    bar = '█' * filled_length + '-' * (100 - filled_length)
+
+    sys.stdout.write('\r |%s| %s%%' % (bar, percents)),
+
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
